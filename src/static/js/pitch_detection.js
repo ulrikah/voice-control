8ef622fccd6305c890c28129ea6fed5a02ac1952
ulrikah <ulrikhalmoy@gmail.com>
2019-10-23 19:19:53 +0200

    Remove Tone from project

--- src/static/js/pitch_detection.js
+++ src/static/js/pitch_detection.js
@@ -12,15 +12,13 @@ Basic Pitch Detection
 let audioContext;
 let mic;
 let pitch;
-let fmOsc;
-let prevMidi;
+let prevNote;
 
 // threshold of micLevel by trial and error
 const MIC_THRESHOLD = 0.01;
 
 function preload() {
-	fmOsc = new Tone.FatOscillator().toMaster();
-  fmOsc.volume.value = -40
+  // stuff that needs to load before setup()
 }
 
 function setup() {
@@ -37,29 +35,34 @@ function startPitch() {
 function modelLoaded() {
   select('#status').html('Model Loaded');
   getPitch();
-  // fmOsc.start()
 }
 
+function freqToNote( frequency ) {
+  var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+  return Math.round( noteNum ) + 69;
+}
+
+function noteToFreq( note ) {
+  return 440 * Math.pow(2,(note-69)/12);
+}
 
 function getPitch() {
   pitch.getPitch(function(err, frequency) {
     if (frequency && typeof frequency === "number") {
-      select('#result').html(frequency);
       
-      const midi = Tone.Frequency(freqToMidi(frequency), 'midi').toMidi()
+      const note = freqToNote( frequency )
+      select('#result').html(note);
     	
-      // We only bother to send a request onChange as of now
-      if (midi && prevMidi != midi){
+      // We only bother to send a request when note is changed as of now
+      if (note && prevNote != note){
 
-        const micLevel = mic.getLevel() // TO DO - add velocity from volume
+        const micLevel = mic.getLevel()
         if (micLevel >= MIC_THRESHOLD)
         {        
-          const midiJson = { "key": midi, "level": micLevel } 
-          sendMidiToServer(midiJson);
-          prevMidi = midi;
-    
-          // Set the frequency of the oscillator to the new value
-          fmOsc.frequency.value = midi;
+          const noteJson = { "key": note, "level": micLevel } 
+          sendMidiToServer(noteJson);
+          prevNote = note;
+          
         }
       }
 
@@ -84,13 +87,4 @@ function sendMidiToServer(midiJson, printResponse = false) {
   }
   http.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
   http.send(JSON.stringify(midiJson))
-}
-
-function mouseClicked() {
-	if (fmOsc.state === "stopped")
-	{
-		fmOsc.start()
-	} else {
-		fmOsc.stop()
-	}
 }
