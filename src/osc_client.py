f19157014684685be30ea427cee879d77541579e
ulrikah <ulrikhalmoy@gmail.com>
2020-01-06 16:43:00 +0100

    Keep track of active clients

--- src/osc_client.py
+++ src/osc_client.py
@@ -6,6 +6,11 @@ class Client:
         self.port = int(port)
         self.client = udp_client.SimpleUDPClient(self.ip, self.port)
 
-    def send_msg(self, key, level, msg_type, channel="/osc"):
+    def send_msg(self, key, level, msg_type, socket_id = "", channel="/osc"):
         msg = (key, level, msg_type)
+
+        # TO DO: add logic for handling socket ID and sequences
+        if socket_id:
+        	print("SOCKET_ID", socket_id)
+
         self.client.send_message(channel, msg)
--- src/server.py
+++ src/server.py
@@ -15,6 +15,8 @@ socketio = SocketIO(app, cors_allowed_origins = "*")
 client = Client(os.getenv("OSC_IP"), os.getenv("OSC_PORT"))
 print("Setting up OSC client at", client.ip, "and port", client.port)
 
+clients = []
+
 @app.route("/")
 def hello():
 	return render_template("index.html")
@@ -45,14 +47,27 @@ def handle_midi(json):
 	msg_type = json['msg_type']
 	print(f"[ key {key} | level {round(level, 3)} | msg_type {msg_type}]")
 	print("\n")
-	client.send_msg(key, level, msg_type)
+	client.send_msg(key, level, msg_type, request.sid)
 	return "Success"
 
-# general event for incoming sends
+# incoming unnamed message events
 @socketio.on('message')
 def handle_message(message):
-	print("\n THIS IS THE SEND EVENT")
 	print('received message: ' + message)
+
+# connection event
+@socketio.on('connect')
+def handle_connect():
+	print("Client connected with ID: " + request.sid)
+	clients.append(request.sid)
+	pass
+
+# disconnection event
+@socketio.on('disconnect')
+def handle_disconnect():
+	print("Client disconnected with ID: " + request.sid)
+	clients.remove(request.sid)
+	pass
 
 if __name__ == '__main__':
 	socketio.run(
